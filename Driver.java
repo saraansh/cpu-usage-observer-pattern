357a1301570c31d68910760fe2616c0bb1b797b0
saraansh <bsaraansh@gmail.com>
2020-01-19 20:58:59 +0530

    Fix using explicit extractor for cpu usage

--- Driver.java
+++ Driver.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
@@ -8,12 +10,11 @@ public class Driver {
         new DemoObserver(subject);
 
         // to read outputs form shell commands
-        ProcessBuilder processBuilder = new ProcessBuilder();
+        final String command = "top -b -d1 -n1";
+        String[] commandSequence = command.split(" ");
         // if multiple paramters are required - modify the command to get those and parse them separately in getSnapshot function
         // also return a dictionary / hashmap for better handling of the multiple parameters
-        final String command = "top -b -d1 -n1|grep -i \"Cpu(s)\"|head -c21|cut -d \' \' -f2|cut -d \'%\' -f1";
-        // System.out.println(command);
-        processBuilder.command(command);
+        ProcessBuilder processBuilder = new ProcessBuilder(commandSequence);
 
         System.out.println("Time for monitoring (in seconds) - ");
         int seconds = Integer.parseInt(br.readLine());
@@ -21,7 +22,6 @@ public class Driver {
         for (int i = 0; i < seconds; i++) {
             subject.setCpuUsage(getSnapshot(processBuilder));
         }
-
     }
     
     public static double getSnapshot(ProcessBuilder processBuilder) {
@@ -30,12 +30,28 @@ public class Driver {
         // return cpuUsage or memUsage as required
         try {
             Process process = processBuilder.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            double cpuUsage = Double.parseDouble(reader.readLine());
-            return cpuUsage;
+            InputStream is = process.getInputStream();
+            String output = "";
+            int value = -1;
+            while ((value = is.read()) != -1) {
+                output += (char) value;
+            }
+            // System.out.println(output);
+            return extractCpuUsage(output);
         } catch (IOException e) {
             // e.printStackTrace();
             System.out.println("[Driver.java] Error in fetching %CPU Usage");
+            return -1;
+        }
+    }
+
+    public static double extractCpuUsage(String str) {
+        Pattern pattern = Pattern.compile("%Cpu\\(s\\):[ ]+[1-9]*[0-9]\\.[0-9][1-9]*");
+        Matcher m = pattern.matcher(str);
+        if (m.find()) {
+            return Double.parseDouble(" " + str.substring(m.start() + 8, m.end()));
+        } else {
+            System.out.println("this.executed");
             return -1;
         }
     }
